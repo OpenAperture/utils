23f118d097bc5bbfc04a16fce17342385450f678
Mike Hoskins <mike.hoskins@perceptivesoftware.com>
2015-08-19 14:34:59 -0500

    added oa_envver apps

--- oa_envver/install_oa_envver.sh
+++ oa_envver/install_oa_envver.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 # Install /etc/my_init.d/00_OA_vars.sh
-curl https://raw.githubusercontent.com/OpenAperture/utils/master/oa_envver/00_OA_Vars.sh -o /etc/my_init.d/00_OA_vars.sh
+curl https://raw.githubusercontent.com/OpenAperture/utils/master/oa_envver/00_OA_vars.sh -o /etc/my_init.d/00_OA_vars.sh
 chmod 755 /etc/my_init.d/00_OA_vars.sh
 
 # Install envver
